7180417dff635c5e144c38742ff3848dea2246b6
sectorworld <sector.hell@gmail.com>
2023-10-19 15:25:42 +0300

    19102023

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+from bs4 import BeautifulSoup
+import requests
+#import bs4
+res=requests.get('https://lesnoi.by')
+with open('tst.html','w') as file_ob:
+    file_ob.write(res.text)
+with open('tst.html','r') as file_ob:
+    src=file_ob.read()
+#print(src)
+soup=BeautifulSoup(src,"lxml")
+#print(soup.text)
+all_a=soup.find_all('a')
+for item in all_a:
+    item_text=item.text
+    item_url=item.get("href")
+    print(f"{item_text}:{item_url}")
+#print(all_a)
+#for link in soup.find_all('a'):
+#    print(link.get('href'))
+#page_h1=soup.find_all("h2")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#for item in src:
+#    print(item.text)
 
+#print(soup.get_text())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#for i in soup.find_all()
